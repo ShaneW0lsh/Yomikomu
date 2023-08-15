8aea8f8ed72b024c3b44dd6d2bb0499f69ec18cb
Shane <shanebetterfaher@gmail.com>
2023-08-16 00:11:01 +0300

    Do some desperate things

--- app/src/main/java/com/shane/yomikomu/MainActivity.kt
+++ app/src/main/java/com/shane/yomikomu/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.shane.yomikomu
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Base64
-import android.webkit.WebView
+import android.os.Environment
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import java.io.FileInputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    //private var selectedURI: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainButton = findViewById<Button>(R.id.main_button)
+        mainButton.setOnClickListener {
+            openFileDialogue()
+        }
+
     }
 
-    private fun loadWebViewData(unencodedHtml: String) {
-        val webBookContents: WebView = findViewById(R.id.web_book_contents)
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data: Intent? = result.data
+                val uri = data?.data
+                unzip(uri, Environment.getExternalStorageDirectory().path + "/Yomikomu/")
+                //val path = uri?.path
+                //Log.i("File's path", path.toString())
+            }
+        }
 
-        val encodedHtml = Base64.encodeToString(unencodedHtml.toByteArray(), Base64.NO_PADDING)
-        webBookContents.loadData(encodedHtml, "text/html", "base64")
+    private fun openFileDialogue() {
+        var data = Intent(Intent.ACTION_GET_CONTENT)
+        data.addCategory(Intent.CATEGORY_OPENABLE)
+        data.type = "application/epub+zip"
+        data = Intent.createChooser(data, "Choose a file")
+        resultLauncher.launch(data)
+    }
+
+    private fun unzip(inputEpub: Uri?, output: String): Boolean {
+        try {
+            val filename: String
+            val inputStream: FileInputStream = applicationContext.contentResolver.openInputStream(inputEpub) as FileInputStream
+
+
+        } catch(e: IOException) {
+            e.printStackTrace()
+            return false
+        }
+        return true
     }
 }
--- app/src/main/java/com/shane/yomikomu/data/Datasource.kt
+++ app/src/main/java/com/shane/yomikomu/data/Datasource.kt
@@ -4,25 +4,29 @@ import android.content.Context
 import android.util.Log
 import com.shane.yomikomu.model.HTMLElement
 import nl.siegmann.epublib.epub.EpubReader
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 import java.nio.charset.StandardCharsets
 
 class Datasource(private val context: Context) {
 
     fun loadHTMLElements(): List<HTMLElement> {
-        TODO("Use parser here instead. You should render only tags inside body tag")
-        val HTMLList = retrieveBookData().split("\n").filterNot { it.isBlank() }
+//        val HTMLList = retrieveBookHTML().split("\n").filterNot { it.isBlank() }
+//        val returnList = mutableListOf<HTMLElement>()
+//
+//        for (elementString in HTMLList)
+//            returnList.add(HTMLElement(elementString))
+
         val returnList = mutableListOf<HTMLElement>()
+        val doc: Document = Jsoup.parse(retrieveBookHTML())
+        val paragraphs: Elements = doc.select("p")
 
-        for (elementString in HTMLList)
-            returnList.add(HTMLElement(elementString))
-
-        Log.d("LIST", returnList[20].toString())
-        Log.d("LIST", returnList[21].toString())
-
+        Log.d("Paragraphs", paragraphs.html().toString())
         return returnList
     }
 
-    private fun retrieveBookData(): String {
+    private fun retrieveBookHTML(): String {
         val book = EpubReader().readEpub(context.assets.open("8.epub"))
 
         var dataBytes = byteArrayOf()
